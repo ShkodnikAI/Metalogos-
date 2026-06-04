12cc95f3c7d63e09c80b3794a0ee2660616e07b8
Z User <z@container>
2026-06-04 14:56:22 +0000

    049359d3-f79d-4fbf-801e-75adfa5ea319

---
+++ metalogos/target/debug/deps/metalogos-3dd4850ff2d456cc.d
@@ -0,0 +1,5 @@
+/home/z/my-project/metalogos/target/debug/deps/metalogos-3dd4850ff2d456cc.d: src/main.rs
+
+/home/z/my-project/metalogos/target/debug/deps/metalogos-3dd4850ff2d456cc: src/main.rs
+
+src/main.rs:

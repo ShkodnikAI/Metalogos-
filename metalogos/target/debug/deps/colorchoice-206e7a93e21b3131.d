5491ff72d02c69f9997763647da39cdd039d2439
Z User <z@container>
2026-06-04 14:56:22 +0000

    049359d3-f79d-4fbf-801e-75adfa5ea319

---
+++ metalogos/target/debug/deps/colorchoice-206e7a93e21b3131.d
@@ -0,0 +1,7 @@
+/home/z/my-project/metalogos/target/debug/deps/colorchoice-206e7a93e21b3131.d: /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.5/src/lib.rs
+
+/home/z/my-project/metalogos/target/debug/deps/libcolorchoice-206e7a93e21b3131.rlib: /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.5/src/lib.rs
+
+/home/z/my-project/metalogos/target/debug/deps/libcolorchoice-206e7a93e21b3131.rmeta: /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.5/src/lib.rs
+
+/home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.5/src/lib.rs:
---
+++ metalogos/target/debug/deps/metalogos-c66774e7ec5d3df6.d
@@ -0,0 +1,10 @@
+/home/z/my-project/metalogos/target/debug/deps/metalogos-c66774e7ec5d3df6.d: src/main.rs src/ast.rs src/builtins.rs src/interpreter.rs src/parser.rs /home/z/my-project/metalogos/src/grammar.pest
+
+/home/z/my-project/metalogos/target/debug/deps/metalogos-c66774e7ec5d3df6: src/main.rs src/ast.rs src/builtins.rs src/interpreter.rs src/parser.rs /home/z/my-project/metalogos/src/grammar.pest
+
+src/main.rs:
+src/ast.rs:
+src/builtins.rs:
+src/interpreter.rs:
+src/parser.rs:
+/home/z/my-project/metalogos/src/grammar.pest:

7b015b52e7b7eaf953bd2b417c4f6bba45c57983
Z User <z@container>
2026-06-04 14:56:22 +0000

    049359d3-f79d-4fbf-801e-75adfa5ea319

---
+++ metalogos/target/debug/deps/metalogos-1fb177876bc62678.d
@@ -0,0 +1,12 @@
+/home/z/my-project/metalogos/target/debug/deps/metalogos-1fb177876bc62678.d: src/lib.rs src/ast.rs src/builtins.rs src/interpreter.rs src/parser.rs /home/z/my-project/metalogos/src/grammar.pest
+
+/home/z/my-project/metalogos/target/debug/deps/libmetalogos-1fb177876bc62678.rlib: src/lib.rs src/ast.rs src/builtins.rs src/interpreter.rs src/parser.rs /home/z/my-project/metalogos/src/grammar.pest
+
+/home/z/my-project/metalogos/target/debug/deps/libmetalogos-1fb177876bc62678.rmeta: src/lib.rs src/ast.rs src/builtins.rs src/interpreter.rs src/parser.rs /home/z/my-project/metalogos/src/grammar.pest
+
+src/lib.rs:
+src/ast.rs:
+src/builtins.rs:
+src/interpreter.rs:
+src/parser.rs:
+/home/z/my-project/metalogos/src/grammar.pest:
---
+++ metalogos/target/debug/deps/metalogos-663be91f0db8af75.d
@@ -0,0 +1,10 @@
+/home/z/my-project/metalogos/target/debug/deps/metalogos-663be91f0db8af75.d: src/lib.rs src/ast.rs src/builtins.rs src/interpreter.rs src/parser.rs /home/z/my-project/metalogos/src/grammar.pest
+
+/home/z/my-project/metalogos/target/debug/deps/metalogos-663be91f0db8af75: src/lib.rs src/ast.rs src/builtins.rs src/interpreter.rs src/parser.rs /home/z/my-project/metalogos/src/grammar.pest
+
+src/lib.rs:
+src/ast.rs:
+src/builtins.rs:
+src/interpreter.rs:
+src/parser.rs:
+/home/z/my-project/metalogos/src/grammar.pest:

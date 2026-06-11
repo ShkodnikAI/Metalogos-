caa7402bc5d19c2a1801e1caedc0fa9326c97b70
Z User <z@container>
2026-06-11 07:17:56 +0000

    f7f01fb3-68d0-4a67-a8a3-b9878b30312b

---
+++ tests/audit_parse_test.rs
@@ -0,0 +1,13 @@
+#[test]
+fn test_parse_mlogserver_from_integration() {
+    let source = r#"
+mlogserver {
+  port: 8080
+  middleware: [session, csrf, security_headers]
+  route "/" method=GET { return "Hello" }
+}
+"#;
+    let decls = metalogos::parser::parse(source).unwrap();
+    eprintln!("Integration parse: {} declarations", decls.len());
+    assert!(decls.len() >= 1);
+}
